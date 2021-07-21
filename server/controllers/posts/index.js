06752e6453e5e6ea1971b1cebffaddfc5631f33e
shpk93 <shpk930924@gmail.com>
2021-07-22 03:52:15 +0900

    ✅Add: 포스트삭제 구현

--- server/controllers/posts/index.js
+++ server/controllers/posts/index.js
@@ -1,3 +1,6 @@
+const { post,user,posts_ingredient } = require('../../models')
+const { isAuthorized } = require('../tokenFunctions');
+
 module.exports ={
     get: (req, res) => {
 
@@ -9,6 +12,28 @@ module.exports ={
 
     },
     delete: (req, res) => {
+      let id = Number(req.params.id)
+      let userInfo = isAuthorized(req)
+      
+      if(userInfo){
+        post.findOne({
+          where: {
+            id,
+          },
+        })
+          .then((data) => {
+            if(userInfo.id === data.dataValues.user_id) {
+              post.destroy({
+                where: {
+                  id
+                }
+              })
+              .then(del => res.status(200).json({"message": "ok"}))
+            }
+            else return res.status(401).json({"message": "Unauthorized request"})
 
+          })
+      }
+      else return res.status(401).json({"message": "Unauthorized request"})
     }
   };
