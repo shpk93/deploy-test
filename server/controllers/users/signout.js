acd0157bff5212829b72a43cef49fe080d6a2b32
HunKimADev <HunKimADev>
2021-07-23 10:01:40 +0900

    💄Format: formatted with prettier

--- server/controllers/users/signout.js
+++ server/controllers/users/signout.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
-    res.cookie("jwt", '', {
-        httpOnly: true,
-        maxAge :1
-      });
-    res.status(200).send({"message": "ok"});
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    maxAge: 1,
+  });
+  res.status(200).send({ message: 'ok' });
 };
