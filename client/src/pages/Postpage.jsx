c83cd82aad54e4ddc59d1084bdbfd8d66fd65f0a
byungminkim <byungminkim12@gmail.com>
2021-07-26 10:49:28 +0900

    ✅Add: Postpage function

--- client/src/pages/Postpage.jsx
+++ client/src/pages/Postpage.jsx
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import RadioMenu from '../components/RadioMenu';
 import CheckMenu from '../components/CheckMenu';
 import axios from 'axios';
+import Textbox from '../components/Textbox';
+import styled from 'styled-components';
+
+const url = process.env.REACT_APP_API_URL;
+axios.defaults.withCredentials = true;
+
+const PostAreaStyle = styled.div`
+  margin-top: 150px;
+  padding: 0;
+  height: 100%;
+`;
 
 function Postpage() {
   const [checkedItems, setCheckedItems] = useState([]);
   const [allMenu, setAllMenu] = useState(null);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [errMessage, setErrMessage] = useState(false);
+
+  const history = useHistory();
 
   useEffect(async () => {
+    //주소 수정해주세요
     const data = await axios.get('https://api.mysubway.ml/menu').then((result) => {
       const arr = [];
       const name = Object.keys(result.data.data);
@@ -20,6 +38,10 @@ function Postpage() {
     });
     setAllMenu(data);
   }, []);
+
+  useEffect(() => {
+    setErrMessage(false);
+  }, [checkedItems]);
 
   const handleRadio = (item) => {
     const name = checkedItems.map((el) => el.name);
@@ -62,9 +84,41 @@ function Postpage() {
     }
   };
 
+  const handlePostSend = () => {
+    const check = checkedItems.reduce((acc, cur) => {
+      return [...acc, cur.name];
+    }, []);
+    if (!check.includes('main')) {
+      setErrMessage('메인을 선택해주세요');
+      return;
+    } else if (!check.includes('bread')) {
+      setErrMessage('빵을 선택해주세요');
+      return;
+    } else if (!check.includes('cheese')) {
+      setErrMessage('치즈를 선택해주세요');
+      return;
+    }
+
+    const menu = checkedItems.reduce((acc, cur) => {
+      if (Array.isArray(cur.id)) {
+        return [...acc, ...cur.id];
+      } else {
+        return [...acc, cur.id];
+      }
+    }, []);
+    const data = {
+      title: title.content,
+      content: content.content,
+      menu,
+    };
+    //완료시 리렌더링되게 바꿔야함
+    axios.post(`${url}posts`, data).then((result) => history.push('/'));
+  };
+
   return (
     allMenu && (
-      <div>
+      <PostAreaStyle>
+        <Textbox name="Title" textData={setTitle} />
         {allMenu.map((el) => {
           const name = Object.keys(el)[0];
           return name === 'main' || name === 'bread' || name === 'cheese' ? (
@@ -81,7 +135,12 @@ function Postpage() {
             </div>
           );
         })}
-      </div>
+        <Textbox name="Contents" textData={setContent} />
+        <div>
+          {errMessage ? <div>{errMessage}</div> : null}
+          <button onClick={handlePostSend}>GO POST</button>
+        </div>
+      </PostAreaStyle>
     )
   );
 }
