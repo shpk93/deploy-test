4af0c9ddaf82b421667faae9565c63f531264c08
byungminkim <byungminkim12@gmail.com>
2021-07-29 11:32:28 +0900

    🐛Fix: Css 수정

--- client/src/components/PostButton.jsx
+++ client/src/components/PostButton.jsx
@@ -4,9 +4,9 @@ import { Switch, Route, Link, useHistory } from 'react-router-dom';
 
 const AsideStyled = styled.div`
   position: fixed;
-  bottom: 110px;
+  bottom: 15%;
   left: 49%;
-  margin: 0 0 0 635px;
+  margin: 0 0 0 35%;
   z-index: 997;
 `;
 
@@ -17,17 +17,17 @@ const LogImg = styled.img`
   overflow: hidden;
 `;
 
-function PostButton() {
-  const openPost = () => {
-    console.log('열려라 참깨');
-  };
-
+function PostButton({ isLogIn }) {
   return (
-    <AsideStyled>
-      <Link to="/post">
-        <LogImg src="../imageFile/subway-Log.png" alt="log" onClick={openPost} />
-      </Link>
-    </AsideStyled>
+    <div>
+      {isLogIn ? (
+        <AsideStyled>
+          <Link to="/post">
+            <LogImg src="../imageFile/subway-Log.png" alt="log" />
+          </Link>
+        </AsideStyled>
+      ) : null}
+    </div>
   );
 }
 
